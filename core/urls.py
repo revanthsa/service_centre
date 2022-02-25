b24d60e7bb23982a9262c67b460c70ca6b78eca6
Revanth S A <revanthlp3@gmail.com>
2022-02-26 00:11:31 +0530

    feat: password reset

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
 from .views import *
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 	path('register/', register_customer.as_view(), name="register"),
 	path('', home, name="home"),
+	path('password_reset/', auth_views.PasswordResetView.as_view(success_url='done/', html_email_template_name='registration/password_reset_email.html'), name="password_reset"),
+	path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+	path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(success_url='/reset/done/', post_reset_login=True), name="password_reset_confirm"),
+	path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
